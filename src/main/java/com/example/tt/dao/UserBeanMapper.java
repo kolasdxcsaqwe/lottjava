71323a41b1876cc14f458d5679ba0ba25d087f0d
4318471pk <4318471pk@gmail.com>
2023-12-11 19:26:42 +0800

    修改

--- src/main/java/com/example/tt/dao/UserBeanMapper.java
+++ src/main/java/com/example/tt/dao/UserBeanMapper.java
@@ -1,7 +1,6 @@
 package com.example.tt.dao;
 
 import com.example.tt.Bean.UserBean;
-import org.apache.ibatis.annotations.Param;
 
 public interface UserBeanMapper {
     int deleteByPrimaryKey(Integer id);
@@ -17,12 +16,4 @@ public interface UserBeanMapper {
     int updateByPrimaryKeyWithBLOBs(UserBean record);
 
     int updateByPrimaryKey(UserBean record);
-
-    UserBean selectByUserId(@Param("userid") String userId);
-
-    UserBean selectByUserAndRoomId(@Param("userName") String userName,@Param("roomId") Integer roomId,@Param("agent") String agent);
-
-    UserBean selectByUserAndRoomIdAndPassword(@Param("userName") String userName,@Param("roomId") Integer roomId,@Param("password") String password,@Param("agent") String agent);
-
-
 }
